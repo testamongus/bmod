8bbe66f3470dec88eb1a2bf65e4162b52017aa4c
Prankapple <bnnaadcscs@gmail.com>
2025-06-05 15:22:49 +0300

    Update sb3-downloader.jsx

--- src/containers/sb3-downloader.jsx
+++ src/containers/sb3-downloader.jsx
@@ -16,7 +16,7 @@ const getProjectTitleFromFilename = fileInputFilename => {
     if (!fileInputFilename) return '';
     // only parse title with valid scratch project extensions
     // (.sb, .sb2, .sb3, .pm, and .pmp)
-    const matches = fileInputFilename.match(/^(.*)(\.sb[23]?|\.pm|\.pmp|\.snail)$/);
+    const matches = fileInputFilename.match(/^(.*)(\.sb[23]?|\.pm|\.pmp|\.snail|\.bm)$/);
     if (!matches) return '';
     return matches[1].substring(0, 100); // truncate project title to max 100 chars
 };
@@ -252,7 +252,7 @@ const getProjectFilename = (curTitle, defaultTitle) => {
     if (!filenameTitle || filenameTitle.length === 0) {
         filenameTitle = defaultTitle;
     }
-    return `${filenameTitle.substring(0, 100)}.snail`;
+    return `${filenameTitle.substring(0, 100)}.bm`;
 };
 
 SB3Downloader.propTypes = {
