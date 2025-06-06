eae1d198ed900fc40bcc1ab9ef73b8e7a2353d42
Prankapple <bnnaadcscs@gmail.com>
2025-06-06 16:15:49 +0300

    Update index.jsx

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -173,6 +173,15 @@ const menuItems = [
     },
 
 
+    {
+        name: 'Meme Extesnion',
+        extensionId: 'https://banana-mod.github.io/MemeExtension/MemeExtension.js',
+        iconURL: 'https://banana-mod.github.io/MemeExtension/memeextension.svg',
+        collaborator: 'Banana-mod',
+        tags: ['banana-mod'],
+        description: 'Get fresh baked memes.',
+        featured: true
+    },
 
     {
         name: '3D',
